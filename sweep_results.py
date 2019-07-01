3695a8da329fc9204df5851064c96964b0d87e9d
Djordje <dorde.grbic@gmail.com>
2019-07-01 14:55:16 +0200

    Script that finds the best performing model on the test.
     Changes to be committed:
    	new file:   sweep_results.py

---
+++ sweep_results.py
@@ -0,0 +1,47 @@
+from os import listdir, path
+from functools import partial
+from main_vis import run
+import torch
+from model import ControllerCombinator
+import pickle
+from statistics import mean
+
+EVOLUTION_DIR = "./trained_models/pulled_from_server/20random_goals4modules20episode_monolith_multiplexor_AVG_FITNESS"
+NUM_EXP = 20
+
+
+def get_model_eval(model_filename):
+    m1_orig = torch.load(model_filename)
+    m1 = ControllerCombinator(2, 4, 100, 2, 2, sees_inputs=False)
+    m1.load_state_dict(m1_orig.state_dict())
+    experiment1_fits = [run(m1, False) for _ in range(NUM_EXP)]
+    experiment1_fits = list(zip(*experiment1_fits))
+    return mean(experiment1_fits[1]), experiment1_fits[1]
+
+
+def main():
+    generation_ids = listdir(EVOLUTION_DIR)
+    join_gen = partial(path.join, EVOLUTION_DIR)
+    gen_paths = map(join_gen, generation_ids)
+    ind_file_names = map(lambda x: "individual_" + x + ".pt", generation_ids)
+    model_paths = [path.join(stem, fn) for (stem, fn) in zip(gen_paths, ind_file_names)]
+
+    avg_fitnesses = list(map(get_model_eval, model_paths))
+    avg_fitnesses = list(zip(*avg_fitnesses))
+
+    max_performance = float("-inf"), None, None  # average fitness, fitness_list, id
+    for m_id, avg, flist in zip(generation_ids, avg_fitnesses[0], avg_fitnesses[1]):
+        if avg > max_performance[0]:
+            max_performance = (avg, flist, m_id)
+
+    print(
+        "best average fitness is {} from the {}th individual".format(
+            max_performance[0], max_performance[2]
+        )
+    )
+    with open("experiment_sweep_best.list", "wb") as pckl_file:
+        pickle.dump(max_performance[1], pckl_file)
+
+
+if __name__ == "__main__":
+    main()
