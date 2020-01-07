Fix the action log probability calculation.
 Changes to be committed:
	modified:   a2c_ppo_acktr/evaluation.py
	modified:   a2c_ppo_acktr/model.py
	modified:   train_test_model.py

a2c_ppo_acktr/evaluation.py
@@ -24,7 +24,7 @@ def evaluate(actor_critic, ob_rms, eval_envs, num_processes,
     done = False
     while not done:
         with torch.no_grad():
-            _, action, _, eval_recurrent_hidden_states = actor_critic.act(
+            _, action, _, eval_recurrent_hidden_states, _ = actor_critic.act(
                 obs,
                 eval_recurrent_hidden_states,
                 eval_masks,

a2c_ppo_acktr/model.py
@@ -56,7 +56,7 @@ class PolicyWithInstinct(nn.Module):
 
 
     def act(self, inputs, rnn_hxs, masks, deterministic=False):
-        value, action, action_log_probs, rnn_hxs = self.policy.act(inputs, rnn_hxs, masks, deterministic)
+        value, action, _, rnn_hxs, dist = self.policy.act(inputs, rnn_hxs, masks, deterministic)
         instinct_action, control = self.instinct(inputs)
 
         controlled_stoch_action = action * control
@@ -64,7 +64,8 @@ class PolicyWithInstinct(nn.Module):
 
         final_action = controlled_stoch_action + controlled_instinct_action
 
-        return value, final_action, action_log_probs, rnn_hxs
+        action_log_probs = dist.log_probs(action)
+        return value, final_action, action_log_probs, rnn_hxs, dist
 
     def get_value(self, inputs, rnn_hxs, masks):
         return self.policy.get_value(inputs, rnn_hxs, masks)
@@ -123,7 +124,7 @@ class Policy(nn.Module):
         action_log_probs = dist.log_probs(action)
         dist_entropy = dist.entropy().mean()
 
-        return value, action, action_log_probs, rnn_hxs
+        return value, action, action_log_probs, rnn_hxs, dist
 
     def get_value(self, inputs, rnn_hxs, masks):
         value, _, _ = self.base(inputs, rnn_hxs, masks)

train_test_model.py
@@ -174,7 +174,7 @@ def train_maml_like_ppo(
         for step in range(num_steps):
             # Sample actions
             with torch.no_grad():
-                value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
+                value, action, action_log_prob, recurrent_hidden_states, _ = actor_critic.act(
                     rollouts.obs[step], rollouts.recurrent_hidden_states[step],
                     rollouts.masks[step])
 