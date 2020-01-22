d24eed22d362d8ca6519003a838a335fd3cc1330
Djordje <dorde.grbic@gmail.com>
2020-01-22 13:50:38 +0100

    Add features to log parsing.
     Changes to be committed:
    	modified:   visualisations/parse_fitness_progress.py

--- visualisations/parse_fitness_progress.py
+++ visualisations/parse_fitness_progress.py
@@ -6,18 +6,19 @@ import glob
 import re
 from os.path import join
 
-def main():
+
+def plot_concatinate_files(files_dir, log_file_list):
     best_in_pop_ptrn = re.compile("best in the population ---->")
     stabilize_ptrn = re.compile("best in the population after stabilization")
+    activation_ptrn = re.compile("activation average")
     float_ptrn = re.compile("-*\d+.\d*")
-    file_dir = "/Users/djgr/code/instincts/modular_rl/trained_models/pulled_from_server/second_phase_instinct/n_deterministic_goals/4goals_lidars_extra_cost"
 
-    parts = [1]
     best_val_generation = []
-    besta_stabile_generation = []
-    for prt in parts:
-        prs_file = f"EVOLUTION_lidar_fixed_part{prt}.log"
-        src_path = join(file_dir, prs_file)
+    best_stabile_generation = []
+    best_activation_generation = []
+
+    for log_file in log_file_list:
+        src_path = join(files_dir, log_file)
 
         with open(src_path, "r") as src_file:
             for log_line in src_file:
@@ -25,20 +26,68 @@ def main():
                 if best_line is not None:
                     val = float_ptrn.findall(log_line)[0]
                     best_val_generation.append(float(val))
+
                 best_stabile_line = stabilize_ptrn.search(log_line)
                 if best_stabile_line is not None:
                     val = float_ptrn.findall(log_line)[0]
-                    besta_stabile_generation.append(float(val))
+                    best_stabile_generation.append(float(val))
+
+                best_activation_line = activation_ptrn.search(log_line)
+                if best_activation_line is not None:
+                    val = float_ptrn.findall(log_line)[0]
+                    best_activation_generation.append(float(val))
+
+    return best_val_generation, best_stabile_generation, best_activation_generation
 
 
-    print(best_val_generation.index(max(best_val_generation)))
-    plt.plot(range(len(best_val_generation)), best_val_generation)
-    plt.plot(range(len(besta_stabile_generation)), besta_stabile_generation, '--')
-    plt.xlabel("generation")
-    plt.ylabel("fitness")
-    plt.title(prs_file)
-    #plt.legend(loc='upper left')
+def plot_graph(graphs, labels, x_lbl, y_lbl, title):
+    for gr, lbl in zip(graphs, labels):
+        plt.plot(range(len(gr)), gr, label=lbl)
+
+    plt.xlabel(x_lbl)
+    plt.ylabel(y_lbl)
+    plt.title(title)
+    plt.legend(loc="lower right")
     plt.show()
+
+
+def main():
+
+    files_dir = "/Users/djgr/code/instincts/modular_rl/trained_models/pulled_from_server/second_phase_instinct/n_deterministic_goals/largeSmallExploration/logs"
+
+    best_vals = []
+    best_activations = []
+    numbers = [
+        plot_concatinate_files(
+            files_dir, [f"EVOLUTION_lidar_largeExp_part{prt}.log" for prt in [1, 2]]
+        ),
+        plot_concatinate_files(
+            files_dir,
+            [f"EVOLUTION_lidar_largeExpGoalReward_part{prt}.log" for prt in [1, 2]],
+        ),
+        plot_concatinate_files(
+            files_dir, [f"EVOLUTION_lidar_smallExp_part{prt}.log" for prt in [1, 2]]
+        ),
+    ]
+
+    numbers = list(zip(*numbers))
+
+    plot_graph(
+        numbers[0],
+        ["large exploration", "large exp + reward for finish", "small exploration"],
+        "generation",
+        "fitness",
+        "best individual fitness",
+    )
+
+    plot_graph(
+        numbers[2],
+        ["large exploration", "large exp + reward for finish", "small exploration"],
+        "generation",
+        "average activation",
+        "best individual average lifetime activation",
+    )
+
 
 if __name__ == "__main__":
     main()
